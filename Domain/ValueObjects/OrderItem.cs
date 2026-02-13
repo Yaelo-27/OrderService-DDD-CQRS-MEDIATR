325c9ccafec987c101558469134e0eb3ec69e6ca
ylituma <yhaelituma@hotmail.com>
2026-02-13 11:19:27 +0100

    change guid product id for int

--- Domain/ValueObjects/OrderItem.cs
+++ Domain/ValueObjects/OrderItem.cs
@@ -4,23 +4,23 @@ namespace Domain.ValueObjects;
 public partial record OrderItem
 {
     private OrderItem() { }
-    private OrderItem(Guid productId, string? productName, int quantity, decimal unitPrice)
+    private OrderItem(int productId, string? productName, int quantity, decimal unitPrice)
     {
         ProductId = productId;
         ProductName = productName;
         Quantity = quantity;
         UnitPrice = unitPrice;
     }
-    public static OrderItem? Create(Guid productId, string? productName, int quantity, decimal unitPrice)
+    public static OrderItem? Create(int productId, string? productName, int quantity, decimal unitPrice)
     {
-        if (productId == Guid.Empty || productName  == null || quantity <= 0 || unitPrice < 0)
+        if (productId <= 0 || productName  == null || quantity <= 0 || unitPrice < 0)
         {
             return null; // Return null if any of the required fields are invalid, ensuring that only valid order items are created.
         }
 
         return new OrderItem(productId, productName, quantity, unitPrice);
     }
-    public Guid ProductId { get; init; }
+    public int ProductId { get; init; }
     public string? ProductName { get; init; }
     public int Quantity { get; init; }
     public decimal UnitPrice { get; init; }
